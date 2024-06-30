34bb9d0eec839cbe438a1ffcf9a886daff329c9f
Shivangi Srivastava <shivangi.juhi@gmail.com>
2024-07-01 01:06:53 +0400

    Demo Project for Microservices Communication- via RestTemplate

--- serviceB/src/main/java/com/example/serviceB/controller/ServiceBController.java
+++ serviceB/src/main/java/com/example/serviceB/controller/ServiceBController.java
@@ -3,14 +3,31 @@ package com.example.serviceB.controller;
 import com.example.serviceB.config.ServiceAClient;
 import com.example.serviceB.domain.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 public class ServiceBController {
+
+    @Value("${rest.user.details.baseUrl}")
+    private String userDetailsURL;
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ServiceBController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     @Autowired
     private ServiceAClient serviceAClient;
@@ -28,4 +45,23 @@ public class ServiceBController {
         return serviceAClient.getUserDetailsFromServiceA(id);
 
     }
+
+    @GetMapping("/dataBaseUsers")
+    public List<UserResponse> getAllUsers(){
+
+        HttpEntity<String> requestEntity = getHttpEntity();
+
+        ResponseEntity<List<UserResponse>>responseEntity = restTemplate.exchange(userDetailsURL, HttpMethod.GET, requestEntity, new ParameterizedTypeReference<>() {
+        });
+        return responseEntity.getBody();
+    }
+
+    private static HttpEntity<String> getHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<String> requestEntity = new HttpEntity<>(headers);
+        return requestEntity;
+    }
+
+
 }
